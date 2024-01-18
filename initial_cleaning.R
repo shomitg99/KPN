063baf7262b66526b95aa5fb6e267b3353e6ea25
shomitg99 <shomitafc99@gmail.com>
2024-01-18 22:28:34 +0100

    nba + meta data merge attempt

--- initial_cleaning.R
+++ initial_cleaning.R
@@ -1,10 +1,39 @@
 # Load packages
 library(ggplot2)
 library(caret)
+library(readxl)
+library(lubridate)
+library(data.table)
+library(progress)
 
 # Load raw data
-nba_served <- read.csv("campaigns_served.csv", header = TRUE, stringsAsFactors = TRUE)
-nba_meta <- read.csv("campaign_metadata.csv", sep=";", stringsAsFactors=TRUE)
-customers <- read.csv("customer_features.csv", header = TRUE, stringsAsFactors = TRUE)
+nba_served <- fread("campaigns_served.csv")
+nba_meta <- fread("campaign_metadata.csv")
+customers <- fread("customer_features.csv")
 
-# Rename customer feature variables
+# Rename customer feature variables - needs to be figured out later (check descriptions)
+new_vars <- read_excel("new_vars.xlsx", col_names = FALSE)
+new_vars <- t(new_vars)
+colnames(customers) <- new_vars
+
+# Create 'month' in customer features and campaigns served
+colnames(customers)[colnames(customers) == "datum"] <- "date"
+customers$date <- as.Date(customers$date)
+table(customers$date)
+customers$month <- month(customers$date)
+
+nba_served$month <- month(nba_served$decision_date)
+
+# Merging datasets (by 'month' and 'nba_name')
+full <- nba_served
+col_nba_meta <- names(nba_meta)
+full[, col_nba_meta] <- NA
+
+pb <- progress_bar$new(total = nrow(full))
+
+for (i in 1:nrow(full)) {
+  pb$tick()
+  full[i, col_nba_meta] <- nba_meta[i,]
+}
+
+pb$close()
